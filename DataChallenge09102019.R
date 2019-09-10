e5065e319dd841987f81e196623480dbd5c947e0
XiaoxuanLin <xiaoxuanlin@uchicago.edu>
2019-09-10 15:50:42 -0400

    fix loading data

--- DataChallenge09102019.R
+++ DataChallenge09102019.R
@@ -1,3 +1,4 @@
 ## INITIAL DATA CHALLENGE FILE 09102019
 
-read.csv('~/U_of_C/QBio/BSD-QBio5/tutorials/reproducibility/data/cole_arthropod_data_1946.csv')
+anthropod <- read.csv('~/U_of_C/QBio/BSD-QBio5/tutorials/reproducibility/data/cole_arthropod_data_1946.csv')
+weevil_egg <- read.csv('~/U_of_C/QBio/BSD-QBio5/tutorials/reproducibility/data/mitchell_weevil_egg_data_1975.csv')
